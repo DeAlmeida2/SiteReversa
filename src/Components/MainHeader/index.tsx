9cedab9cef6505bc044804d970c4a9411a54605c
Andre Almeida <extern.andre.almeida2@volkswagen.com.br>
2023-01-17 10:46:54 -0300

    Atualização

---
+++ src/Components/MainHeader/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MainHeader: React.FC = () => {
+    return (
+        <h1>Teste Main Header</h1>
+    );
+}
+
+export default MainHeader;
---
+++ src/Pages/sobre/index.jsx
@@ -0,0 +1,7 @@
+export default function sobre() {
+    return (
+        <div>
+            <h1>Site Reversa</h1>
+        </div>
+    )
+}
